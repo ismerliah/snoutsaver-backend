b898f12a12ef363ccf6ab7b904479cc96b68a75c
Yanisa Thaenthong <mook.yanisa@gmail.com>
2024-08-28 23:58:57 +0700

    add routers

---
+++ snoutsaver/snoutsaver/routers/__init__.py
@@ -0,0 +1,9 @@
+from . import users
+from . import records
+from . import categories
+
+
+def init_router(app):
+    app.include_router(users.router)
+    app.include_router(records.router)
+    app.include_router(categories.router)
