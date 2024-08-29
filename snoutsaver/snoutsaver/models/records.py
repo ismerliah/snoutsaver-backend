bd1c87ba9a3498e4f6a15aa04b97fcde26c347a1
Harmoナイア ( SIRAPOP YIWLEK ) <105147522+LazyHarmo@users.noreply.github.com>
2024-08-29 21:07:19 +0700

    feat(records): add CRUD

--- snoutsaver/snoutsaver/models/records.py
+++ snoutsaver/snoutsaver/models/records.py
@@ -1,33 +1,38 @@
 import datetime
-from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, ConfigDict
 from typing import Optional
 
 from sqlmodel import Field, SQLModel
 
 
-class Record(BaseModel):
+class BaseRecord(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
     user_id: int
     amount: float
     currency: str
-
     type: str
-
 
 class Records(BaseModel):
     id: int
 
 
-class CreateRecord(Record):
+class CreateRecord(BaseRecord):
     pass
 
+class UpdatedRecord(BaseRecord):
+    type: Optional[str] = None
+    amount: float
+    currency: str
 
-class DBRecord(Record, SQLModel):
+class DBRecord(BaseRecord, SQLModel, table=True):
     __tablename__ = "records"
     id: int = Field(default=None, primary_key=True)
-
+    user_id = int
+    amount: float
+    currency: str
+    type: str
     record_date: datetime.datetime = Field(default=datetime.datetime.now)
 
 class RecordList(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-    records: list[Record]
+    records: list[Records]
--- snoutsaver/snoutsaver/routers/records.py
+++ snoutsaver/snoutsaver/routers/records.py
@@ -1,53 +1,102 @@
+from email import message
 from fastapi import APIRouter, HTTPException, Depends, Query
 
 from typing import Optional,Annotated
 
+from flask import session
+from flask_login import AnonymousUserMixin
 from sqlmodel import Field, SQLModel, select, func, Session
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from snoutsaver.models.records import Record,Records
-
 from .. import models
+from .. import deps
 
 router = APIRouter(tags=["Record"], prefix="/records")
 
 
-records = []
-
 # Create
-@router.post("/records/")
-async def create_record(record: Record):
-    record.id = len(records) + 1
-    records.append(record)
-    return record
+@router.post("/records")
+async def create_record(
+    record: models.BaseRecord,
+    current_user: Annotated[models.users, Depends(deps.get_current_user)],
+    session: Annotated[AsyncSession, Depends(models.get_session)],
+    ) -> models.Records | None:
+    data = await session.get(current_user.id)
+    if not data:
+        raise HTTPException(status_code=404, detail=" data not found ;( ")
+    
+    data = await session.execute(
+        select(models.DBRecord).where(models.DBRecord.name == record.name)
+    )
+    
+    db_record = models.DBRecord.model_validate(record)
+    session.add(db_record)
+    await session.commit()
+    await session.refresh(db_record)
+
+    return models.Records.model_validate(db_record)
 
 # Read 
-@router.get("/records/")
-async def read_records():
-    return Records(__root__=records)
+@router.get("/records")
+async def read_all_records(
+    session: Annotated[AsyncSession, Depends(models.get_session)]
+) -> models.RecordList:
+    
+    result = await session.exec(select(models.DBRecord))
+    records = result.all()
 
-# Read a single record by ID
-@router.get("/records/{record_id}")
-async def read_record(record_id: int):
-    for record in records:
-        if record.id == record_id:
-            return record
-    raise HTTPException(status_code=404, detail="Record not found")
+    return models.RecordList.model_validate(
+        dict(items=records)
+    )
+
+# Read record by ID
+@router.get("/{record_id}")
+async def read_record(
+    record_id: int,
+    session: Annotated[AsyncSession, Depends(models.get_session)]
+    ) -> models.Records:
+    
+    db_record = await session.get(models.DBRecord, record_id)
+    if not db_record:
+        raise HTTPException(status_code=404, detail="record not found")
+
+    return models.Records.model_validate
 
 # Update
-@router.put("/records/{record_id}")
-async def update_record(record_id: int, record: Record):
-    for i, existing_record in enumerate(records):
-        if existing_record.id == record_id:
-            records[i] = record
-            return record
-    raise HTTPException(status_code=404, detail="Record not found")
+@router.put("/{record_id}")
+async def update_record(
+    record_id: int, 
+    record: models.UpdatedRecord,
+    session: Annotated[AsyncSession, Depends(models.get_session)]
+    ) -> models.Records:
+    
+    data = record.model_dump()
+    db_record = await session.get(models.DBRecord, record_id)
+
+    if not db_record:
+        raise HTTPException(status_code=404, detail="record not found")
+    
+    db_record.sqlmodel_update(data)
+    session.add(db_record)
+    await session.commit()
+    await session.refresh(db_record)
+
+    return models.Records.model_validate(db_record)
 
 # Delete
-@router.delete("/records/{record_id}")
-async def delete_record(record_id: int):
-    for i, existing_record in enumerate(records):
-        if existing_record.id == record_id:
-            del records[i]
-            return {"message": "Record deleted"}
-    raise HTTPException(status_code=404, detail="Record not found")
+@router.delete("/{record_id}")
+async def delete_record(
+    record_id: int,
+    session: Annotated[AsyncSession, Depends(models.get_session)]
+    ) -> dict:
+
+    db_record = await session.get(models.DBRecord, record_id)
+
+    if not db_record:
+        raise HTTPException(status_code=404, detail="record not found")
+    
+    await session.delete(db_record)
+    await session.commit()
+
+    return dict(message="Delete record success")
+  
